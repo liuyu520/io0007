b8f4b102c8a0e199fb8f0deb6a6ad472ebfc6af6
huangweii <huangweii@chanjet.com>
2016-04-09 13:43:12 +0800

    增加过滤请求参数防xss攻击的util类

--- src/main/java/com/string/widget/util/XSSUtil.java
+++ src/main/java/com/string/widget/util/XSSUtil.java
@@ -2,7 +2,7 @@ package com.string.widget.util;
 
 public class XSSUtil {
     /***
-     * 没有对& 处理,因为参数值可能是:<br >
+     * 没有对& 和斜杠/进行处理,因为参数值可能是:<br >
      * http://hbjltv.com/info/request?username=huang&password=admin<br>
      * 用于过滤请求参数
      *
@@ -13,11 +13,35 @@ public class XSSUtil {
         if (ValueWidget.isNullOrEmpty(input)) {
             return input;
         }
+        System.out.println("before xss:\t" + input);
         input = input.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
 //        input = input.replaceAll("\\(", "&#40;").replaceAll("\\)", "&#41;");//HTML标准中没有转码
         input = input.replaceAll("'", "&#39;");
         input = input.replaceAll("\"", "&quot;");
 //        input=input.replaceAll("alert","");
+        System.out.println("after xss:\t" + input);
+        return input;
+    }
+
+    /***
+     * 没有对& 和斜杠/进行处理,因为参数值可能是:<br >
+     * http://hbjltv.com/info/request?username=huang&password=admin<br>
+     * 用于过滤请求参数
+     *
+     * @param input
+     * @return
+     */
+    public static String deleteXSS(String input) {
+        if (ValueWidget.isNullOrEmpty(input)) {
+            return input;
+        }
+        System.out.println("before xss:\t" + input);
+        input = input.replaceAll("<", "").replaceAll(">", "");
+//        input = input.replaceAll("\\(", "&#40;").replaceAll("\\)", "&#41;");//HTML标准中没有转码
+        input = input.replaceAll("'", "");
+        input = input.replaceAll("\"", "");
+//        input=input.replaceAll("alert","");
+        System.out.println("after xss:\t" + input);
         return input;
     }
 }
